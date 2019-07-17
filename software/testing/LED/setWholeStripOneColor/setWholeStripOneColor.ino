49e95859d7f3af0bad1110b4c81b8b9da424be15
lperovich <lperovich@gmail.com>
2019-07-17 18:58:34 -0400

    more LED code updates

--- software/testing/LED/setWholeStripOneColor/setWholeStripOneColor.ino
+++ software/testing/LED/setWholeStripOneColor/setWholeStripOneColor.ino
@@ -1,37 +1,59 @@
+//Code notation by Talia and Laura July 2019
 //Make sure to plug in a battery (don't just connect by USB to the computer) to make sure the LEDs are sufficiently powered
 // Simple strand test for Adafruit Dot Star RGB LED strip.
 
+/////////////////////////////////////// DEFINE THE VARIABLES
 #include <Adafruit_DotStar.h>
 #include <SPI.h>         
 
-#define NUMPIXELS 44 // Number of LEDs in strip
+// Number of LEDs in strip
+#define NUMPIXELS 44 
 
 // Here's how to control the LEDs from any two pins:
 #define DATAPIN    13
 #define CLOCKPIN   11
 Adafruit_DotStar strip(NUMPIXELS, DATAPIN, CLOCKPIN, DOTSTAR_BRG);
 
-void setup() {
-
-  strip.begin(); // Initialize pins for output
-  strip.show();  // Turn all LEDs off ASAP
-}
-
-/////////////////////////////////////// OUR STRIPS ARE #GGRRBB not #RRGGBB ////////////////////////////////////////////////////////////////////////////////
-
+//YOU CAN ADD NEW COLORS HERE IN TWO WAYS
+// in one way you look up the hex code of the color here (https://html-color-codes.info/) and write it out
+//// NOTE: OUR STRIPS ARE #GGRRBB not #RRGGBB, so the order will be a little different //////
 uint32_t green = 0xFF0000;      // green using hex 
 uint32_t blue = 0x0000FF;      // blue using hex
 uint32_t yellow = 0xFFFF00;      // yellow using hex
 uint32_t white = 0xFFFFFF;        // white using hex
+//Create a new color using this template:
+//uint32_t white = 0xFFFFFF;        // white using hex
 
+// in the second way, you tell the LEDs how much of each color you want to use
+// each of the three numbers in this line can be any number between 0 and 255
+// one number represents how much red, one represents how much green, and one represents how much blue 
+uint32_t blue = strip.Color(0, 0, 255);   // blue using rgb
+uint32_t red = strip.Color(0, 255, 0);    // red using rgb
+uint32_t green = strip.Color(255, 0, 0);    // green using rgb
+uint32_t purple = strip.Color(0, 255, 255);   // purple using rgb
+uint32_t purple = strip.Color(0, 255, 255);   // purple using rgb
+//Create a new color using this template:
+//uint32_t purple = strip.Color(0, 255, 255);   // purple using rgb
+
+
+/////////////////////////////////////// SET EVERYTHING UP
+void setup() {
+  //Get the lights ready to turn out
+  strip.begin(); // Initialize pins for output
+  strip.show();  // Turn all LEDs off ASAP
+}
+
+
+
+/////////////////////////////////////// RUN THE CODE
 void loop() {
-
-  uint32_t blue = strip.Color(0, 0, 255);   // blue using rgb
-  uint32_t red = strip.Color(0, 255, 0);    // red using rgb
-  uint32_t green = strip.Color(255, 0, 0);    // green using rgb
-  uint32_t purple = strip.Color(0, 255, 255);   // purple using rgb
-
+  //Put the color that you want for the LEDs here, based on the color names that you created above. The LED strand will turn this color
   strip.fill(purple);
   strip.show();
-  
+
+ //you can create two consecutive colors using delays
+ //delay(500);
+ //strip.fill(purple);
+ //strip.show();
+ 
 }
