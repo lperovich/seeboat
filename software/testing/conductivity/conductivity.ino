ce1e2560ab1d348e773dba0382696d67bad32284
lperovich <lperovich@gmail.com>
2019-12-13 15:57:53 -0500

    analog decimals adding hopefully

--- software/testing/conductivity/conductivity.ino
+++ software/testing/conductivity/conductivity.ino
@@ -17,6 +17,8 @@ void setup() {
   Serial.begin(9600);
   pinMode(power, OUTPUT);
   pinMode(sensor, INPUT);
+  //default is to 10, get more with the feather
+  analogReadResolution(12);
 }
 
 ///////////////////////////////////// GET VALUES
@@ -28,7 +30,10 @@ void loop() {
   //analog read goes from 0-1023; our range of voltage goes from 0 to 3.3, so scale things accordingly to get a voltage value
   //But we're working from half ground, so the lowest we'll actually every read off of analogue read is half of 1023
   //map this into the full voltage range (e.g. 1023/2 should be zero volts; 1023 should go to 3.3 volts)
-  float voltage = mapFloat(val, 1023/2, 1023, 0, 3.3);
+  //for resoltuion of 10
+//  float voltage = mapFloat(val, 1023/2, 1023, 0, 3.3);
+//for resoltuion of 12
+  float voltage = mapFloat(val, 4095/2, 4095, 0, 3.3);
 
   //NOTE: the conductivity code in the SeeBoat Feather code also adjust for the temperature (this impacts conductivity)
 
@@ -36,7 +41,7 @@ void loop() {
   float conductivity = voltToCondRes12(voltage); //for resistor = 1.2 kohm
   //float conductivity = voltToCondRes12(voltage); //for resistor = 1.0 kohm
 
-  Serial.println(voltage);
+  Serial.println(voltage,4);
 
 }
   // map() function except for floats
